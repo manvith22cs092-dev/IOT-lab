e5e3cdff7df9bc859a2f1781671087b484fc09e2
manvith22cs092-dev <manvith.22cs092@sode-edu.in>
2025-09-01 15:54:35 +0530

    lab 3 experiment using LDR

---
+++ lab3.ino
@@ -0,0 +1,19 @@
+int ldr1=0;
+void setup() {
+  // put your setup code here, to run once:
+   pinMode(2,INPUT);
+   pinMode(5,OUTPUT);
+   Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  ldr1=digitalRead(2);
+  Serial.print("Intensity:\n");
+  Serial.print(ldr1);
+  if(ldr1==HIGH){
+   digitalWrite(5,HIGH);
+  }else{
+    digitalWrite(5,LOW);
+   
+  }
